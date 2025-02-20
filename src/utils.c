38ab4fa52ebd318f9f38d469603e5cee8c0f43c9
Nathalia Vitoria Buchholz <nbuchhol@c1r5p3.42sp.org.br>
2025-02-20 01:48:08 -0300

    refactor[utils.c]: key and mouse event handling

---
+++ src/utils.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nbuchhol <nbuchhol@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/20 00:45:00 by nbuchhol          #+#    #+#             */
+/*   Updated: 2025/02/20 01:32:55 by nbuchhol         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/so_long.h"
+
+int	key_handle(int keycode)
+{
+	if (keycode == 65307)
+		exit(0);
+	return (0);
+}
+
+int	mouse_handle(t_classMlx *mlx)
+{
+	mlx_destroy_window(mlx->mlx, mlx->win);
+	exit(0);
+	return (0);
+}
