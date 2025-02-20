b97962b70d89f20704bd911a0af287d43c97b78f
Nathalia Vitoria Buchholz <nbuchhol@c1r5p3.42sp.org.br>
2025-02-19 21:29:29 -0300

    Refactor game_utils.c: Move put_map function to static scope
    The put_map function was moved to static scope to improve encapsulation and prevent unintended access from other files. This change helps to organize the code and make it more maintainable.

--- src/game_utils.c
+++ src/game_utils.c
@@ -6,11 +6,65 @@
 /*   By: nbuchhol <nbuchhol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 17:05:29 by nbuchhol          #+#    #+#             */
-/*   Updated: 2025/02/18 15:00:23 by nbuchhol         ###   ########.fr       */
+/*   Updated: 2025/02/19 21:27:44 by nbuchhol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
+
+void	initialize_sprites(t_classMlx *mlx)
+{
+	int		px;
+	char	*bg;
+	char	*char_still;
+	char	*collectable;
+	char	*exit;
+	char	*wall;
+
+	px = 32;
+	bg = "../assets/bg.xpm";
+	char_still = "../assets/char_still.xpm";
+	collectable = "../assets/collectable.xpm";
+	exit = "../assets/exit.xpm";
+	wall = "../assets/bg.xpm";
+	mlx->bg = mlx_xpm_file_to_image(mlx->mlx, bg, &px, &px);
+	mlx->char_still = mlx_xpm_file_to_image(mlx->mlx, char_still, &px, &px);
+	mlx->collectable = mlx_xpm_file_to_image(mlx->mlx, collectable, &px, &px);
+	mlx->exit = mlx_xpm_file_to_image(mlx->mlx, exit, &px, &px);
+	mlx->wall = mlx_xpm_file_to_image(mlx->mlx, wall, &px, &px);
+}
+
+static void	put_image(t_classMlx *mlx, void *img, int x, int y)
+{
+	mlx_put_image_to_window(mlx->mlx, mlx->win, img, x * 32, y * 32);
+}
+
+static void	put_map(t_game *game, t_classMlx *mlx)
+{
+	int	x;
+	int	y;
+
+	y = 0;
+	while (y < game->map_h)
+	{
+		x = 0;
+		while (x < game->map_w)
+		{
+			if (game->map[y][x] == '0')
+				put_image(mlx, mlx->bg, x, y);
+			else if (game->map[y][x] == '1')
+				put_image(mlx, mlx->bg, x, y);
+			else if (game->map[y][x] == 'P')
+				put_image(mlx, mlx->char_still, x, y);
+			else if (game->map[y][x] == 'E')
+				put_image(mlx, mlx->exit, x, y);
+			else if (game->map[y][x] == 'C')
+				put_image(mlx, mlx->collectable, x, y);
+			x++;
+		}
+		y++;
+	}
+}
 
 void	open_screen(t_game *game)
 {
@@ -27,51 +81,9 @@ void	open_screen(t_game *game)
 		exit(error_handling(5));
 	mlx_class.win = mlx_new_window(mlx_class.mlx, win_w, win_h, "Hello world!");
 	initialize_sprites(&mlx_class);
-	put_map(&game, &mlx_class);
+	put_map(game, &mlx_class);
 	mlx_loop(mlx_class.mlx);
 }
-
-void	initialize_sprites(t_classMlx *mlx)
-{
-	mlx->bg = mlx_xpm_file_to_image(mlx->mlx, "../assets/bg.xpm", 32, 32);
-	mlx->char_still = mlx_xpm_file_to_image(mlx->mlx, "../assets/char_still.xpm", 32, 32);
-	mlx->collectable = (mlx->mlx, "../assets/collectable.xpm", 32, 32);
-	mlx->exit = (mlx->mlx, "../assets/exit.xpm", 32, 32);
-	mlx->wall = (mlx->mlx, "../assets/bg.xpm", 32, 32);
-}
-
-static void	put_image(t_classMlx *mlx, void *img, int x, int y)
-{
-	mlx_put_image_to_window(mlx->mlx, mlx->win, img, x * 32, y * 32);
-}
-
-void	put_map(t_game *game, t_classMlx *mlx)
-{
-	int	x;
-	int	y;
-
-	y = 0;
-	while (y < game->map_h)
-	{
-		x = 0;
-		while (x < game->map_w)
-		{
-			if (game->map[y][x] == '0')
-				put_image(mlx, mlx->bg, x, y);
-			else if (game->map[y][x] == '0')
-				put_image(mlx, mlx->bg, x, y);
-			else if (game->map[y][x] == 'P')
-				put_image(mlx, mlx->char_still, x, y);
-			else if (game->map[y][x] == 'E')
-				put_image(mlx, mlx->exit, x, y);
-			else if (game->map[y][x] == 'C')
-				put_image(mlx, mlx->collectable, x, y);
-			x++;
-		}
-		y++;
-	}
-}
-
 
 void	close_game(int fd, char **map, int error_code)
 {
